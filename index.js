b2506f17c6061fa2b113d8353f8c5ebdc0b30096
immanuel-savvy <immanuelsavvy@gmail.com>
2025-10-24 11:26:51 -0700

    com2

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import Repos from "@godprotocol/repositories";
 import Oracle from "./Oracle.js";
 
-import dotenv from "dotenv";
-dotenv.config();
-
 let mirror = new Repos();
 
 let oracle = new Oracle(mirror);
